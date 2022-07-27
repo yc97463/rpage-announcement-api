7bc851ac188fedbccaadca47c69f4c4b02d75dac
yc97463 <youualan87@gmail.com>
2022-07-27 16:17:57 +0800

    feat(src/crawler):

--- src/crawler.py
+++ src/crawler.py
@@ -1,11 +1,13 @@
 import requests
-
-def fetch(url):
-    response = requests.get(url)
-    response = requests.get(url, cookies={'over18': '1'})  # 一直向 server 回答滿 18 歲了 !
-    return response
+from bs4 import BeautifulSoup
 
 url = 'https://www.smhs.kh.edu.tw/p/403-1000-24-1.php'
-resp = fetch(url)  # step-1
+resp = requests.get(url)
+soup = BeautifulSoup(resp.text, 'html5lib')
 
-print(resp.text) # result of setp-1
+links = soup.find_all('div', class_='mtitle')
+print(links)
+# for link in links:
+#     if 'href' in link.attrs:
+#         print(link['href'])
+# print("done", soup, links, sep='\n')
