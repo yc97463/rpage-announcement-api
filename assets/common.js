ffda1e527fe8d6b99c072faba111b24c18f5a5d8
yc97463 <youualan87@gmail.com>
2022-08-03 00:01:24 +0800

    fix(assets/common): fix url too long and cache problems

--- assets/common.js
+++ assets/common.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 let latest = urlParams.get('load');
 let rcg = urlParams.get('rcg');
 console.log(latest);
-let now = new Date();
+let now = new Date().getTime();
 if (latest == null) {
     if(rcg!=null){
         window.location.href = window.location.href+"&load="+now;
